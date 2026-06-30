2c238baa3b0b2262e08d4b94c26cd7a16bbff776
Viktor Stoianovych <vitiochek@gmail.com>
2026-06-30 12:11:04 +0300

    Health core

--- Assets/Project Files/Game/Scripts/Player/HealthBehavior.cs
+++ Assets/Project Files/Game/Scripts/Player/HealthBehavior.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
@@ -9,59 +7,107 @@ namespace Watermelon
     {
         [SerializeField] HealthbarBehaviour healthbar;
 
+        [Space]
+        [SerializeField] bool enableRegeneration;
+        [SerializeField, Min(0)] float regenerationDelay = 5f;
+        [SerializeField, Min(0)] float regenerationPerSecond = 1f;
+
         public float CurrentHealth { get; private set; }
         public float MaxHealth { get; private set; }
 
         public bool IsDepleted => CurrentHealth <= 0;
         public bool IsFull => CurrentHealth >= MaxHealth;
+        public bool IsRegenerationEnabled => enableRegeneration;
 
         public bool ShowOnChange { get; set; }
+
         public bool HideOnFull { get; set; }
+
+        public event SimpleCallback HealthChanged;
+        public event SimpleCallback Damaged;
+        public event SimpleCallback Depleted;
+        public event SimpleCallback Restored;
+
+        private bool isInitialised;
+        private bool isHealthbarVisibilityAllowed;
+        private float regenerationAllowedTime;
 
         public void Initialise(float maxHealth)
         {
-            MaxHealth = maxHealth;
-            CurrentHealth = maxHealth;
+            Initialise(maxHealth, maxHealth);
+        }
 
-            healthbar.Initialise(transform, this, false);
+        public void Initialise(float maxHealth, float currentHealth)
+        {
+            MaxHealth = Mathf.Max(0, maxHealth);
+            CurrentHealth = Mathf.Clamp(currentHealth, 0, MaxHealth);
+
+            isHealthbarVisibilityAllowed = false;
+            regenerationAllowedTime = Time.time + regenerationDelay;
+
+            HealthChanged -= RefreshHealthbar;
+            HealthChanged += RefreshHealthbar;
+
+            if (healthbar != null)
+                healthbar.Initialise(transform, this, false);
+
+            isInitialised = true;
+
+            NotifyHealthChanged();
         }
 
         private void Update()
         {
-            healthbar.FollowUpdate();
+            if (healthbar != null)
+                healthbar.FollowUpdate();
+
+            if (!isInitialised || !enableRegeneration || regenerationPerSecond <= 0)
+                return;
+
+            if (IsDepleted || IsFull || Time.time < regenerationAllowedTime)
+                return;
+
+            Add(regenerationPerSecond * Time.deltaTime);
         }
 
         public void Add(float value)
         {
-            CurrentHealth += value;
-            if (CurrentHealth > MaxHealth)
-            {
-                CurrentHealth = MaxHealth;
-                if (HideOnFull) healthbar.DisableBar();
-            }
+            if (value <= 0 || MaxHealth <= 0)
+                return;
 
-            healthbar.RedrawHealth();
+            var previousHealth = CurrentHealth;
+            CurrentHealth = Mathf.Min(CurrentHealth + value, MaxHealth);
+
+            if (Mathf.Approximately(previousHealth, CurrentHealth))
+                return;
+
+            NotifyHealthChanged();
+            Restored?.Invoke();
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void AddPercent(float value)
         {
-            Add(MaxHealth / 100f *  value);
+            Add(MaxHealth / 100f * value);
         }
 
         public void Subtract(float value)
         {
-            CurrentHealth -= value;
+            if (value <= 0 || IsDepleted)
+                return;
 
-            if (CurrentHealth <= 0)
-            {
-                CurrentHealth = 0;
-                healthbar.DisableBar();
-            } else
-            {
-                if (ShowOnChange && healthbar.IsDisabled) healthbar.EnableBar();
-                healthbar.RedrawHealth();
-            }
+            var previousHealth = CurrentHealth;
+            CurrentHealth = Mathf.Max(0, CurrentHealth - value);
+            regenerationAllowedTime = Time.time + regenerationDelay;
+
+            if (ShowOnChange)
+                isHealthbarVisibilityAllowed = true;
+
+            NotifyHealthChanged();
+            Damaged?.Invoke();
+
+            if (previousHealth > 0 && IsDepleted)
+                Depleted?.Invoke();
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -70,29 +116,68 @@ namespace Watermelon
             Subtract(MaxHealth / 100f * value);
         }
 
+        public void SetCurrentHealth(float value)
+        {
+            CurrentHealth = Mathf.Clamp(value, 0, MaxHealth);
+            regenerationAllowedTime = Time.time + regenerationDelay;
+
+            NotifyHealthChanged();
+        }
+
         public void Restore()
         {
+            if (IsFull)
+            {
+                NotifyHealthChanged();
+                return;
+            }
+
             CurrentHealth = MaxHealth;
-            healthbar.RedrawHealth();
+
+            NotifyHealthChanged();
+            Restored?.Invoke();
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Show()
         {
-            healthbar.RedrawHealth();
-            healthbar.EnableBar();
+            isHealthbarVisibilityAllowed = true;
+            RefreshHealthbar();
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Hide()
         {
-            healthbar.DisableBar();
+            isHealthbarVisibilityAllowed = false;
+            RefreshHealthbar();
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void ForceHide()
         {
-            healthbar.ForceDisable();
+            isHealthbarVisibilityAllowed = false;
+
+            if (healthbar != null)
+                healthbar.ForceDisable();
+        }
+
+        private void NotifyHealthChanged()
+        {
+            HealthChanged?.Invoke();
+        }
+
+        private void RefreshHealthbar()
+        {
+            if (healthbar == null)
+                return;
+
+            healthbar.OnHealthChanged();
+
+            var shouldBeVisible = isHealthbarVisibilityAllowed && CurrentHealth > 0 && CurrentHealth < MaxHealth;
+            if (shouldBeVisible)
+                healthbar.EnableBar();
+            else
+                healthbar.DisableBar();
         }
     }
 }
